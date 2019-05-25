4a0e8e4d96ab48ff9797917c3bc9a9da4eeefd4e
Adel <adel.adel@vodafone.com>
2019-05-26 00:59:04 +0200

    Add backend structure and add configuration of mongodb atlas

---
+++ server/app.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const translationsRoutes = require("../backend/routes/translations");
+const app = express();
+// const mongoosastic = require('mongoosastic');
+
+
+
+
+
+
+
+mongoose
+  .connect(
+    'mongodb+srv://Adel:UV4iiLHeosO08J12@tms-koroa.mongodb.net/test?retryWrites=true'
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+
+
+// var translationsSchema = new mongoose.Schema({
+//   source: {
+//     type: String
+//   },
+//   target: {
+//     type: String
+//   }
+// });
+// translationsSchema.plugin(mongoosastic, {
+//   hosts: [
+//     'localhost:9200',
+//   ]
+// });
+
+// var translations = mongoose.model('translations', translationsSchema),
+//   stream = translations.synchronize(),
+//   count = 0;
+// stream.on('data', function (err, doc) {
+//   count++;
+// });
+// stream.on('close', function () {
+//   console.log('indexed ' + count + ' documents!');
+// });
+// stream.on('error', function (err) {
+//   console.log(err);
+// });
+
+
+
+
+
+
+
+
+
+
+// app.use("/api/translations", translationsRoutes);
+
+
+
+
+
+
+
+
+
+app.get('/', function (req, res, next) {
+    var passedVariable = req._parsedUrl.query;
+
+    translations.search({
+      query_string: {
+        query: passedVariable
+      }
+    },
+    function (err, results) {
+      results.hits.hits.map((element) => {
+        return res.status(200).json({
+          values:element
+        })
+      })
+    });
+  });
+
+
+module.exports = app;
